2d78eb6767d1a1cd0d214b4afd1aea9abf7f2d1d
iltaf-ai <iltafshahni@gmail.com>
2026-09-25 15:54:11 +0500

    Prediction model inegreated

---
+++ app/routes/predict.py
@@ -0,0 +1,16 @@
+from fastapi import HTTPException , Depends , status , APIRouter
+from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
+from app.model import Predictions
+from app.schema import PredictionCreate
+from fastapi import UploadFile
+from app.depencies import get_current_user
+from sqlalchemy.orm  import Session
+from app.database import get_db
+
+prediction_router = APIRouter()
+
+@prediction_router.post("/prediction")
+def prediction(user : PredictionCreate , current_user = Depends(get_current_user) , db:Session = Depends(get_db)):
+    
