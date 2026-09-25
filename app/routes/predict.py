65089557141225dfa0070459cc3050f7668b08b3
iltaf-ai <iltafshahni@gmail.com>
2026-09-25 16:11:34 +0500

    Full fastapi integreated with model

--- app/routes/predict.py
+++ app/routes/predict.py
@@ -1,19 +1,51 @@
-from fastapi import HTTPException , Depends , status , APIRouter
-from fastapi.responses import HTMLResponse
-from fastapi.requests import Request
-from fastapi.templating import Jinja2Templates
-from app.model import Predictions
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
 from app.schema import PredictionCreate
-from fastapi import UploadFile
-from app.depencies import get_current_user
-from sqlalchemy.orm  import Session
+from app.model import Predictions
 from app.database import get_db
+from app.depencies import get_current_user
+from app.notebooks import Pipeline_model
 
 prediction_router = APIRouter()
 
+
 @prediction_router.post("/prediction")
-def prediction(user : PredictionCreate , current_user = Depends(get_current_user) , db:Session = Depends(get_db)):
-    Square_Footage = input("Entre you Square_Footage : ")
-    Num_Bedrooms = input("Entre you Num_Bedrooms: ")
-    Num_Bathrooms = input("Entre you Num_Bedrooms: ")
-    
+def prediction(
+    user: PredictionCreate,
+    current_user=Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+
+    new_input = [[
+        user.Square_Footage,
+        user.Num_Bedrooms,
+        user.Num_Bathrooms,
+        user.Year_Built,
+        user.Lot_Size,
+        user.Garage_Size,
+        user.Neighborhood_Quality
+    ]]
+
+    predicted_price = Pipeline_model.predict(new_input)[0]
+
+    new_prediction = Predictions(
+        Square_Footage=user.Square_Footage,
+        Num_Bedrooms=user.Num_Bedrooms,
+        Num_Bathrooms=user.Num_Bathrooms,
+        Year_Built=user.Year_Built,
+        Lot_Size=user.Lot_Size,
+        Garage_Size=user.Garage_Size,
+        Neighborhood_Quality=user.Neighborhood_Quality,
+        Predicted_Price=predicted_price,
+        user_id=current_user.id
+    )
+
+    db.add(new_prediction)
+    db.commit()
+    db.refresh(new_prediction)
+
+    return {
+        "message": "Prediction saved successfully",
+        "predicted_price": predicted_price
+    }
